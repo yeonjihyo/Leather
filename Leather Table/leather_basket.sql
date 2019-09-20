c050361b98e0d8a0e2c21078b51b6e35d032207a
yeonjihyo <jihyo828>
2019-09-20 17:19:25 +0900

    leather table 수정

--- Leather Table/leather_basket.sql
+++ Leather Table/leather_basket.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `leather` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `leather`;
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: leather
 -- ------------------------------------------------------
@@ -9,7 +9,7 @@ USE `leather`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,7 +23,7 @@ USE `leather`;
 
 DROP TABLE IF EXISTS `basket`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `basket` (
   `basket_no` int(11) NOT NULL AUTO_INCREMENT,
   `basket_product_code` varchar(45) NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE `basket` (
   KEY `basket_member_id_idx` (`basket_member_id`),
   CONSTRAINT `basket_member_id` FOREIGN KEY (`basket_member_id`) REFERENCES `member` (`member_id`),
   CONSTRAINT `basket_product_code` FOREIGN KEY (`basket_product_code`) REFERENCES `product` (`product_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `basket` (
 
 LOCK TABLES `basket` WRITE;
 /*!40000 ALTER TABLE `basket` DISABLE KEYS */;
-INSERT INTO `basket` VALUES (91,'thread007','bbb123123',0,99000,3,'I'),(92,'thread006','bbb123123',0,33000,1,'I'),(93,'thread007','bbb123123',0,33000,1,'I'),(94,'thread004','bbb123123',0,66000,2,'I'),(95,'thread007','bbb123123',0,66000,2,'I'),(96,'thread006','bbb123123',0,33000,1,'I'),(97,'thread007','aaa123123',0,33000,1,'D'),(98,'tool004','aaa123123',0,18000,1,'D'),(99,'tool003','bbb123123',0,6500,1,'I'),(100,'thread007','aaa123123',0,66000,2,'D'),(101,'thread007','aaa123123',0,33000,1,'I'),(102,'tool003','aaa123123',0,6500,1,'I'),(103,'tool004','aaa123123',0,18000,1,'I'),(104,'thread006','aaa123123',0,66000,2,'D'),(105,'thread007','aaa123123',0,66000,2,'I'),(106,'thread005','aaa123123',0,99000,3,'I'),(107,'thread005','aaa123123',0,66000,2,'I');
+INSERT INTO `basket` VALUES (91,'thread007','bbb123123',0,99000,3,'I'),(92,'thread006','bbb123123',0,33000,1,'I'),(93,'thread007','bbb123123',0,33000,1,'I'),(94,'thread004','bbb123123',0,66000,2,'I'),(95,'thread007','bbb123123',0,66000,2,'I'),(96,'thread006','bbb123123',0,33000,1,'I'),(99,'tool003','bbb123123',0,6500,1,'I'),(108,'thread007','aaa123123',0,66000,2,'I'),(109,'tool003','aaa123123',0,6500,1,'I'),(110,'tool001','aaa123123',0,29000,1,'I');
 /*!40000 ALTER TABLE `basket` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-20  0:30:18
+-- Dump completed on 2019-09-20 17:19:01
--- Leather Table/leather_order.sql
+++ Leather Table/leather_order.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `leather` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `leather`;
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: leather
 -- ------------------------------------------------------
@@ -9,7 +9,7 @@ USE `leather`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,12 +23,12 @@ USE `leather`;
 
 DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `order` (
   `order_num` int(11) NOT NULL AUTO_INCREMENT,
   `order_member_id` varchar(16) DEFAULT NULL,
   `order_product_code` varchar(45) NOT NULL,
-  `order_progress` varchar(10) DEFAULT '결제대기중',
+  `order_progress` varchar(10) DEFAULT '결제완료',
   `order_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `deliverycost` int(11) DEFAULT NULL,
   `order_total` int(11) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE `order` (
   KEY `order_product_code_idx` (`order_product_code`),
   CONSTRAINT `order_member_id` FOREIGN KEY (`order_member_id`) REFERENCES `member` (`member_id`),
   CONSTRAINT `order_product_code` FOREIGN KEY (`order_product_code`) REFERENCES `product` (`product_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,6 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (16,'aaa123123','thread005','결제대기중','2019-09-19 14:50:47',NULL,99000,3,'I'),(17,'aaa123123','thread005','결제대기중','2019-09-19 14:50:47',NULL,66000,2,'I');
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -61,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-20  0:30:19
+-- Dump completed on 2019-09-20 17:19:01
